0543dd1d4bbf8a071a66cdf5d0ad68f42a3ab88d
Dariush Abbasi <dariushem@yahoo.com>
2023-01-17 12:24:43 +0330

    update initcontent url

--- app/js/rtlmd.js
+++ app/js/rtlmd.js
@@ -1,6 +1,6 @@
 (function(){
 
-	var initUrl = 'http://dariubs.github.io/rtlmd/initcontent.md';
+	var initUrl = 'https://rtlmd.codecast.ir/initcontent.md';
 
 	$(document).ready(function(){
 		if(localStorage!='' && localStorage['content']!=null){
